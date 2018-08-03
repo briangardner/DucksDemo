9b0d91156125ca2fd90b0f2fc962d048e9bc93c6
Brian Gardner <1761566+briangardner@users.noreply.github.com>
2018-08-03 13:16:36 -0400

    Added Motor Powered Swimming

---
+++ Ducks.Library/Behaviors/SwimBehavior/MotorPoweredSwimBehavior.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ducks.Library.Interfaces.Behaviors;
+
+namespace Ducks.Library.Behaviors.SwimBehavior
+{
+    class MotorPoweredSwimBehavior : ISwimBehavior
+    {
+        public void DoSwim()
+        {
+            Console.WriteLine("Vroom Vroom!");
+        }
+    }
+}
